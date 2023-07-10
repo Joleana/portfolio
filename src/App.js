0bbde980844d1264722f12c70fa6ba72a2af8991
Joleana <joleana@Joleanas-MacBook-Pro.local>
2023-07-10 16:53:49 +0200

    refactor css package for scss, log metrics

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import './App.scss';
+
+
+
+import Home from './pages/Home/Home'
+import About from './pages/About/About'
+import Projects from './pages/Projects/Projects'
+import Contact from './pages/Contact/Contact'
+
+
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <Switch>
+          <Route exact path='/' component={ Home } />
+          <Route path='/about' component={ About } />
+          <Route path='/projects' component={ Projects } />
+          <Route path='/contact' component={ Contact } />
+        </Switch>
+      </Router>
     </div>
   );
 }
