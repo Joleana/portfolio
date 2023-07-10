30b838cacb355f3f5e4e0afd587746fa96bd134f
Joleana <joleana@Joleanas-MacBook-Pro.local>
2023-07-10 16:59:14 +0200

    init components as display and content pages

---
+++ src/pages/Contact/Contact.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Contact() {
+    return (
+        <section className="contact">
+            <p>Joleana Shurley</p>
+        </section>
+    );
+}
+
+export default Contact;
---
+++ src/pages/Projects/Projects.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Projects() {
+    return (
+        <section className="projects">
+            <h2>the projects</h2>
+        </section>
+    );
+}
+
+export default Projects;
