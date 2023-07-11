2aff206d75886be6fc8e5b0052ca5280b1de2444
Joleana <joleana@Joleanas-MacBook-Pro.local>
2023-07-11 09:08:34 +0200

    refactor path for page components and wraps Router in index

--- src/App.js
+++ src/App.js
@@ -1,27 +1,21 @@
 import React from 'react';
 import { Header, Footer } from './components'; // parent directory 'src', objects within components
+import { Home, About, Projects, Contact } from './pages'; // parent directory 'src', objects within components
 
 import './App.scss';
 
-import Home from './pages/Home/Home';
-import About from './pages/About/About';
-import Projects from './pages/Projects/Projects';
-import Contact from './pages/Contact/Contact';
-
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { Switch, Route } from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
-      <Router>
         <Header />
           <Switch>
-            <Route exact path="/" component={Home} />
-            <Route path="/about" component={About} />
-            <Route path="/projects" component={Projects} />
-            <Route path="/contact" component={Contact} />
+            <Route exact path="/" component={ Home } />
+            <Route path="/about" component={ About } />
+            <Route path="/projects" component={ Projects } />
+            <Route path="/contact" component={ Contact } />
           </Switch>
-      </Router>
         <Footer />
     </div>
   );
