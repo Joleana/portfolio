0931b12f92889b2d5f08f1e68c05c570b36ff41c
Joleana <joleana@Joleanas-MacBook-Pro.local>
2023-07-11 05:19:41 +0200

    exports components as default Header, Navbar, Footer

---
+++ src/components/index.jsx
@@ -0,0 +1,9 @@
+import Header from './Header/Header'; // in current (components) folder
+import Navbar from './Navbar/Navbar'; // get export
+import Footer from './Footer/Footer';
+
+export {
+  Header,
+  Navbar,
+  Footer
+}
