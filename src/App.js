b10129cf6cec9f698f23bcb12c6e578e29681b67
Joleana <joleana@Joleanas-MacBook-Pro.local>
2023-07-11 05:12:03 +0200

    Include Header under Router, footer outside of Router

--- src/App.js
+++ src/App.js
@@ -1,13 +1,12 @@
 import React from 'react';
+import { Header, Footer } from './components'; // parent directory 'src', objects within components
+
 import './App.scss';
 
-
-
-import Home from './pages/Home/Home'
-import About from './pages/About/About'
-import Projects from './pages/Projects/Projects'
-import Contact from './pages/Contact/Contact'
-
+import Home from './pages/Home/Home';
+import About from './pages/About/About';
+import Projects from './pages/Projects/Projects';
+import Contact from './pages/Contact/Contact';
 
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
@@ -15,13 +14,15 @@ function App() {
   return (
     <div className="App">
       <Router>
-        <Switch>
-          <Route exact path='/' component={ Home } />
-          <Route path='/about' component={ About } />
-          <Route path='/projects' component={ Projects } />
-          <Route path='/contact' component={ Contact } />
-        </Switch>
+        <Header />
+          <Switch>
+            <Route exact path="/" component={Home} />
+            <Route path="/about" component={About} />
+            <Route path="/projects" component={Projects} />
+            <Route path="/contact" component={Contact} />
+          </Switch>
       </Router>
+        <Footer />
     </div>
   );
 }
