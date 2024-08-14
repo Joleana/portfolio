3e1a1aaa96fbbe0b0f0c8c117595bf213ec9b788
Joleana <joleanashurley@gmail.com>
2024-08-14 19:53:16 +0200

    Refactor scss entry point and add cards to display Projects

--- src/pages/Projects/Projects.jsx
+++ src/pages/Projects/Projects.jsx
@@ -1,11 +1,16 @@
 import React from 'react';
 
-import './Projects.scss';
-
 function Projects() {
     return (
         <section className="projects">
             <h2>the projects</h2>
+            <div id='project-container'>
+                <div className='card'>
+                    <div className='project-image'>
+
+                    </div>
+                </div>
+            </div>
         </section>
     );
 }
