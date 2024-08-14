1ef8e996fdeecbd5273779149fa062f97d48aae5
Joleana <joleanashurley@gmail.com>
2024-08-14 19:33:25 +0200

    Dynamically change footer style based on selected page

--- src/App.js
+++ src/App.js
@@ -1,22 +1,24 @@
 import React from 'react';
-import { Header, Footer } from './components'; // parent directory 'src', objects within components
+import { Header, Footer } from './components/Common'; // parent directory 'src', objects within components
 import { Home, About, Projects, Contact } from './pages'; // parent directory 'src', objects within components
-
-import './App.scss';
-
-import { Switch, Route } from 'react-router-dom';
+import { Switch, Route, useLocation } from 'react-router-dom';
 
 function App() {
+  const location = useLocation();
+  console.log('Location Pathname:', location.pathname);
+
   return (
     <div className="App">
         <Header />
-          <Switch>
-            <Route exact path="/" component={ Home } />
-            <Route path="/about" component={ About } />
-            <Route path="/projects" component={ Projects } />
-            <Route path="/contact" component={ Contact } />
-          </Switch>
-        <Footer />
+          <main className='content'>
+            <Switch>
+              <Route exact path="/" component={ Home } />
+              <Route path="/about" component={ About } />
+              <Route path="/projects" component={ Projects } />
+              <Route path="/contact" component={ Contact } />
+            </Switch>
+          </main>
+        <Footer currentPage={location.pathname} />
     </div>
   );
 }
