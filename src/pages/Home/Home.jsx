e58971bb064e8fb9ecb46a0a376f30c8a43a94f0
Joleana <joleanashurley@gmail.com>
2024-08-14 19:57:24 +0200

    Refactor scss entry point and add animation component to Home page

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,13 +1,20 @@
 import React from 'react';
+import Goo from 'gooey-react';
 
-import './Home.scss';
 
-function Home() {
-    return (
-        <section className="home">
-            <p>Home!</p>
-        </section>
-    );
-} 
+const Home = () => {
+  return (
+    <section className="home">
+      <Goo intensity="strong">
+        <svg width="192" height="192">
+          <g style={{ animation: 'left 5s linear infinite' }}>
+            <circle style={{ animation: 'right 2s linear infinite' }} cx="34%" cy="37%" fill="orchid" r="32" />
+            <circle cx="63%" cy="63%" fill="mediumorchid" r="32" />
+          </g>
+        </svg>
+      </Goo>
+    </section>
+  );
+};
 
 export default Home;
